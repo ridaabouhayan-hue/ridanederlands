01c7083cfc68737bc378d82936b25cb597ddc017
ridaabouhayan-hue <rida.abouhayan@gmail.com>
2026-05-27 17:37:12 +0200

    Transcribe 14 student recordings for B-Alfa group and fix transcripts dashboard CSS warning

--- audio/groepen/index.js
+++ audio/groepen/index.js
@@ -10,7 +10,78 @@ window.groepenAudioData = {
   "NS": [],
   "B-A1": [],
   "B-A2": [],
-  "B-Alfa": [],
+  "B-Alfa": [
+    {
+      "filename": "Birgul Dursel 27-5.ogg",
+      "path": "../audio/groepen/B-Alfa/Birgul Dursel 27-5.ogg",
+      "transcript": "Wat is jouw naam? Mijn naam is Dursa. Wat is jouw achternaam? Mijn achternaam is Temeldema. Wat is jouw adres? Noor-en-Bugestrat. Wat is jouw postcode? 633KNN. In welke gemeente woon je? Wie is jouw contactperson bij de gemeente? Hansen Berthem. Wat is jouw geboorte datum? December 50, 19. Hoe oud ben jij? 30, 40. In welk land kom je? In Oude Turkije. Dank u wel."
+    },
+    {
+      "filename": "Mariama Mina 27-5.ogg",
+      "path": "../audio/groepen/B-Alfa/Mariama Mina 27-5.ogg",
+      "transcript": "Wat is je naam? Mina. Wat is je achternaam? Patricia. Wat is je adres? Fenixland, voor inderdaag. Wat is je postcode? Postcode 1650, voor inderdaag. X, X. In welke commenta woon jij? Ik weet niet. Eindhoven. Wie is jouw contactperson bij de commenta? Jij weet niet? Nee. Wat is jouw geboorte datum? 2001, 21 maart. Hoe oud ben jij? In 20. Welke land kom jij? Afgharistan. Goed."
+    },
+    {
+      "filename": "Mina Mariama 27-5.ogg",
+      "path": "../audio/groepen/B-Alfa/Mina Mariama 27-5.ogg",
+      "transcript": "Wat is je naam? Mijn naam is Mariano. Wat is je naam? Mijn naam is Damba. Wat is je adres? Kamilia Straat, 28. Wat is je paspoort? 5644 CN. In welke Chameleon wil jij? In Eindhoven. Wat is mijn contactperson? Beide Chameleons. Simona Evers. Wat is jouw geboortje daadzame? 8 november 1997. Hoe oud ben jij? Ik ben 28 jaar. In welke landen kom je? Ik kom uit Gambia."
+    },
+    {
+      "filename": "Ikram Hafida 20-5 Huis.ogg",
+      "path": "../audio/groepen/B-Alfa/Ikram Hafida 20-5 Huis.ogg",
+      "transcript": "Woon jij in een huis of een appartement? Ik woon in een appartement in een huis met een tuin. Heb jij een balkon? Ik heb geen balkon. Laat je tuin groeien? Nee, ik heb een klein tuin. Met wie woon jij? Ik woon alleen. En jij? Ik woon met mijn man. Ik heb geen kinderen. Is jouw beurt leuk? Ja, het is rustig. Ik ben in het park dichtbij, in jouw beurt. Ja, mijn beurt is ook leuk. Het bos is dichtbij."
+    },
+    {
+      "filename": "Nursel Meryem 20-5 Huis.ogg",
+      "path": "../audio/groepen/B-Alfa/Nursel Meryem 20-5 Huis.ogg",
+      "transcript": "Woon je in een huis of een appartement? Ik woon in een appartement. Een je? Ik woon in een huis met een tuin. Heb je een balkon? Ik heb twee balkons. Twee balkons. Ja, ik heb twee balkons. Is jouw tuin groot? Ik heb een tuin, die is groot. Wie woon jij? Ik woon met mijn man. En jij? Ik woon met mijn man. Is jouw buurt leuk? Ik woon alleen. Is jouw buurt leuk? Ik woon met mijn man. Is jouw buurt leuk? Ik woon buurt leuk. Ja. Ja, het is rustig. Er is een park, dit is een jouw buurt. Mijn buurt is ook leuk. Het bos is dichtbij."
+    },
+    {
+      "filename": "Dursel Birgul 20-5 Huis.ogg",
+      "path": "../audio/groepen/B-Alfa/Dursel Birgul 20-5 Huis.ogg",
+      "transcript": "Ik woon in een huis. Kein appartement, een groot huis. Mijn huis geen balkon, gewoon eentje. Ja, ik heb een balkon. Is jouw tuin groot? Mijn huis, groot huis. Nee, de tuin is klein. Met wie woon jij? Is groot. Ik woon met, ah nee."
+    },
+    {
+      "filename": "Dursel Mina 20-5 Huis.ogg",
+      "path": "../audio/groepen/B-Alfa/Dursel Mina 20-5 Huis.ogg",
+      "transcript": "Ik heb een woonhuis in een appartement. Geen appartement, mijn huis, groot huis. Heb jij een time op een balkon? Geen balkon en thuis. Welke kamers heb jij? Kierkamers, kierkamers. Welke kamers van je? Mijn woonkamers. Met wie woon jij? Met mijn enkele man. Is jouw huis groot? Ja, mijn huis is groot. Klaar? Klaar."
+    },
+    {
+      "filename": "Ikram Birgul 6 mei.ogg",
+      "path": "../audio/groepen/B-Alfa/Ikram Birgul 6 mei.ogg",
+      "transcript": "Wat drink jij alvast? Eh, ik drink koffie. Wat vind jij lekker? Ik vind eh, ik vind pizza lekker. Waar koop jij eten? Eh, ik koek eh, keb met rijst. Kun jij koken? Ja. Ehm."
+    },
+    {
+      "filename": "Nursel 6 mei.ogg",
+      "path": "../audio/groepen/B-Alfa/Nursel 6 mei.ogg",
+      "transcript": "Wat drink je ochtends? Ik drink tea. Wat eet je vanavond? Pille. Ik eet pasta. Wat vind je lekker? Pizza. Ik vind pizza lekker. Wat vind je niet lekker? Ik vind patat niet lekker. Wat vind je niet lekker? Ik vind de market niet lekker. Wat vind je niet lekker? Pizza."
+    },
+    {
+      "filename": "Mina Mariama 6 mei.ogg",
+      "path": "../audio/groepen/B-Alfa/Mina Mariama 6 mei.ogg",
+      "transcript": "Wat drink jij ochtend? Ik drink melk ochtend. Wat eet jij vandaag? Ik eet rice en kek in het midden. Ik eet vandaag kebbeljepalauw. Wat vind jij lekker? Kiplekers. Ik vind het rass lekker. Wat vind jij niet lekker? Ik vind het rass lekker. Wat vind jij niet lekker? Ik vind pizza niet lekker. Doner niet lekker. Waar koop jij eten? Ik eet eten bij het Albertain restaurant. Ik eet Albertain al die jaar. Ken jij koken? Ja, ik kook ook."
+    },
+    {
+      "filename": "Meryem Dursel 6 mei.ogg",
+      "path": "../audio/groepen/B-Alfa/Meryem Dursel 6 mei.ogg",
+      "transcript": "Ik drink koffie. Ik eet rijst met vis. Ik vind pizza lekker. Wat is het koffie? Ik koffie eet in de supermarkt. Wat is het koffie? Ik koffie pizza."
+    },
+    {
+      "filename": "Nadia Hafida 6 mei.ogg",
+      "path": "../audio/groepen/B-Alfa/Nadia Hafida 6 mei.ogg",
+      "transcript": "Wat drink jij sochtends? Ik drink thee en jij? Ik drink koffie. Wat eet jij vanavond? Ik eet rijst met keb. Lekker! Kan jij koken? Ja, een beetje."
+    },
+    {
+      "filename": "Saloua Hafida 27-5.ogg",
+      "path": "../audio/groepen/B-Alfa/Saloua Hafida 27-5.ogg",
+      "transcript": "Wat is jouw naam? Mijn naam is Hafidah. Wat is jouw achternaam? Bosjeh. Wat is jouw adres? Laureus 78. Wat is jouw boestcode? 5628KK Eindhoven. En welke kentie woon jij? Eindhoven. Wat is jouw kultuur? Vier meken. Wat is jouw geboorn? 2 juli 1982. Hoe oud ben jij? 43. In welke land kom jij? In Marokko."
+    },
+    {
+      "filename": "Nursel Meryem 27-5.ogg",
+      "path": "../audio/groepen/B-Alfa/Nursel Meryem 27-5.ogg",
+      "transcript": "Wat is jouw naam? Mijn naam is Meryem. Wat is jouw achternaam? Mijn achternaam is Temizbas. Wat is jouw adres? Mijn adres is generaal van Neenatenstraat. Wat is jouw postcode? Mijn postcode is 5622H. In welke gemeente woon je? In een woongemeente in Toven. Wie is jouw contactperson bij de gemeente? Mijn contactperson is bij de gemeente. Wat is jouw geboorte datum? Mijn geboorte datum is 3 april 2976. Hoe oud ben je? Ik ben 49 jaar oud. Uit welk land kom je? Ik kom uit Turkije."
+    }
+  ],
   "B-Z-route": [],
   "B-ONA": []
 };
